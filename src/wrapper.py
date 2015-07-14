9f9f6dac644ca24eee624418793fee25fe7d5ff2
Neotrace <DanielJankowski@rub.de>
2015-07-14 09:41:21 +0200

    added modular help

--- src/wrapper.py
+++ src/wrapper.py
@@ -37,11 +37,12 @@ class module_wrapper(object):
     def get_response(self, cmd):
         cmd = re.sub('@.*', '', cmd)
         if cmd.startswith('/hilfe'):
-            return 'Diese Kommandos verstehe ich :)\n\n' \
-                    + '/hilfe - diese Hilfe\n' \
-                    + '/calc [Term] - Rechnet den Term aus(kein Punkt-vor-Strich/keine Klammern)\n' \
-                    + '/9gag - sendet ein zufälliges 9gag Meme\n' \
-                    + '/lsw - Plugin der Linux statt Windows Community'
+            return self.get_help()
+            #return 'Diese Kommandos verstehe ich :)\n\n' \
+            #        + '/hilfe - diese Hilfe\n' \
+            #        + '/calc [Term] - Rechnet den Term aus(kein Punkt-vor-Strich/keine Klammern)\n' \
+            #        + '/9gag - sendet ein zufälliges 9gag Meme\n' \
+            #        + '/lsw - Plugin der Linux statt Windows Community'
         elif cmd.startswith('/calc') and self.modules['calc'] == 'True':
             return self.calc(self.rm_command(cmd))
         elif cmd.startswith('/9gag') and self.modules['9gag'] == 'True':
@@ -74,11 +75,31 @@ class module_wrapper(object):
             elif not cmd.startswith(''):
                 a = 1
         elif cmd.startswith('/'):
-            return 'Ungültiger Befehl! Diese Kommandos verstehe ich :)\n\n' \
-                    + '/hilfe - diese Hilfe\n' \
-                    + '/calc [Term] - Rechnet den Term aus(kein Punkt-vor-Strich/keine Klammern)\n' \
-                    + '/9gag - sendet ein zufälliges 9gag Meme\n' \
-                    + '/lsw - Plugin der Linux statt Windows Community'
+            return 'Ungültiger Befehl! ' + self.get_help()
+
+    def get_help(self):
+        help = 'Diese Befehle verstehe ich ;)\n'
+        for i in self.modules:
+            if self.modules[i] == 'True':
+                if i == 'Facebook':
+                    help += '\n/lsw Facebook: Bekomme die Links zu unserer Facebook Gruppe und Seite.'
+                elif i == 'Thema':
+                    help += '\n/lsw Thema: Finde heraus, was das aktuelle Thema des Monats ist.'
+                elif i == 'Distro':
+                    help += '\n/lsw Distro: Finde heraus, welches die aktuelle Distro des Monats ist.'
+                elif i == 'Mumble':
+                    help += '\n/lsw Mumble: Bekomme Infos über unsereren Mumble Server.'
+                elif i == '9gag':
+                    help += '\n/9gag - sendet ein zufälliges 9gag Meme'
+                elif i == 'Forum':
+                    help += '\n/lsw: Bekomme den Link zu unserer Homepage.'
+                elif i == 'calc':
+                    help += '\n/calc [Term] - Rechnet den Term aus(kein Punkt-vor-Strich/keine Klammern)'
+                elif i == 'FAQ':
+                    help += '\n/lsw FAQ: Siehe unsere Antworten auf beliebte Fragen.'
+                elif i == 'Version':
+                    help += '\n/lsw Version: Bekomme Infos über das Plugin selbst.'
+        return help
 
     def rm_command(self, inp):
         exp = re.sub('\/[a-zA-Z]*\s', '', inp)
